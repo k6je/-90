e84424265186981046378f1075962c78ffda1631
Kaje INC <97894113+k6je@users.noreply.github.com>
2023-03-10 23:12:12 +0300

    Add files via upload

---
+++ kaje.py
@@ -0,0 +1,26 @@
+import os
+import random
+import socket
+print('''
+
+
+                             
+api: HTTP_HOST('177.81.91.187')
+NOT : GEREKLİ HERŞEY APİYE YÜKLÜDÜR APİ KISMINI SAKIN AMA SAKIN ELLEMEYİNİZ
+______________________________________________
+
+Kullanılan Numara: +90 551 772 19 81
+______________________________________________
+
+NOT : HER ENTERLADIĞINIZDA 1 SMS İLERİ GİDİCEK
+______________________________________________
+
+GELEN SMSLER:
+
+	''')
+sms = input("ILLEGALPLATFORMCO > Deneme sms")
+sms = input("BİM > BİM İZMİR/BORNOVA BÖLGESİNDEKİ BİM ŞUBESİNDEN ALDIĞINIZ KODUNUZ B2BB")
+sms = input("TRENDYOL > ALIŞVERİŞ YAPTIĞINIZ İÇİN TEŞEKKÜRLER.")
+sms = input("+7 172 381 291 129 > Deneme Kaje.")
+sms = input("VODAFONE > Vodafone çevirdiğiniz çarktan çıkan haftalık 10gb internet paketi Hattınıza yüklenmiştir Keyifli kullanımlar.")
+sms = input("INSTAGRAM > GİRİŞ KODUNUZ 173166")
